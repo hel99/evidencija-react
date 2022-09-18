23341ea31026647164a45e441ba831ef217fe115
AnjaDrobnjak1 <drobnjakanaaa@gmail.com>
2022-09-18 17:49:18 +0200

    Dodato log out dugme i za admin korisnika

--- src/komponente/Nav.jsx
+++ src/komponente/Nav.jsx
@@ -1,12 +1,34 @@
 import { Link } from 'react-router-dom'
+import Swal from 'sweetalert2'
+import { useNavigate } from 'react-router-dom'
+
 
 const Nav = () => {
+
+    const navigate = useNavigate()
+
+    const logout = () => {
+
+        localStorage.removeItem('email')
+
+        Swal.fire({
+            icon: 'info',
+            title: 'Login message',
+            text: 'You have been successfully logged out!'
+        })
+
+        navigate('/')
+
+    }
+
+
     return (
         <div>
             <nav className="nav-m">
                 <Link to="/prisustva" className='nav-m-link'>Prisustva</Link>
                 <Link to="/kasnjenja" className='nav-m-link'>Kašnjenja</Link>
                 <Link to="/zaposleni" className='nav-m-link'>Zaposleni</Link>
+                <button onClick={logout} className="btn btn-dark" id="admin-logout-button">LOG OUT</button>
             </nav>
         </div>
     )
